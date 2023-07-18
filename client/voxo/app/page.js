6f690cb3d4d18cc5e34521dfdfbb372e7b7d548c
Lakshay <lakshayyadav2006@gmail.com>
2023-07-18 21:07:14 +0530

    Created the hero section.

--- client/voxo/app/page.js
+++ client/voxo/app/page.js
@@ -1,11 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Navbar from "../components/Navbar"
+import Image from "next/image";
+import styles from "./page.module.css";
+import Navbar from "../components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-        <Navbar />
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Navbar/>
+            <Hero/>
+        </div>
+    );
 }
---
+++ client/voxo/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "../styles/Hero.module.css";
+import Image from "next/image";
+import image2 from "../public/image 2.png";
+
+const Hero = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.hero}>
+                <div>
+                    <h1 className={styles.heading}>Creating Artful <br/> Digital Experiences</h1>
+                    <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for
+                        previewing layouts and visual mockups. </p>
+                </div>
+                <Image src={image2} className={styles.mainImage} alt={"abstract pattern"}/>
+            </div>
+
+            <div className={styles.servicesHolder}>
+                <hr />
+                <div className={styles.services}>
+                    <p>design</p>
+                    <p>development</p>
+                    <p>seo</p>
+                </div>
+                <hr />
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
